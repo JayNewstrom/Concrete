9d022c5cc9a5dd04ea0c7366701124687339dbba
Jay Newstrom <jaynewstrom@gmail.com>
2018-09-16 18:15:32 -0500

    Use roboelectric for UI and android tests.

--- concrete-testing/src/test/java/com/jaynewstrom/concretetesting/ConcreteWallTest.kt
+++ concrete-testing/src/test/java/com/jaynewstrom/concretetesting/ConcreteWallTest.kt
@@ -1,7 +1,7 @@
 package com.jaynewstrom.concretetesting
 
 import android.view.LayoutInflater
-import androidx.test.InstrumentationRegistry
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.runner.AndroidJUnit4
 import com.jaynewstrom.concrete.Concrete
 import org.fest.assertions.api.Assertions.assertThat
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith
 class ConcreteWallTest {
     @Test fun ensureContextCreatedFromWallCanBeUsedToInject() {
         val wall = Concrete.pourFoundation(DaggerContextTestComponent.create())
-        val context = wall.createContext(InstrumentationRegistry.getTargetContext())
+        val context = wall.createContext(ApplicationProvider.getApplicationContext())
         val target = ContextTestTarget()
         Concrete.getComponent<ContextTestComponent>(context).inject(target)
         assertThat(target.injectedString).isSameAs("Context test string")
@@ -21,7 +21,7 @@ class ConcreteWallTest {
     @Test fun whenViewIsCreatedWithLayoutInflaterFromChildContextEnsureItCanBeInjectedWithContext() {
         val wall = Concrete.pourFoundation(DaggerContextTestComponent.create())
         val child = wall.stack(ContextChildTestBlock(wall.component))
-        val context = child.createContext(InstrumentationRegistry.getTargetContext())
+        val context = child.createContext(ApplicationProvider.getApplicationContext())
         val exampleView = LayoutInflater.from(context).inflate(R.layout.example, null) as ExampleView
         assertThat(exampleView.injectedChildString).isSameAs("I'm the child")
         assertThat(exampleView.injectedString).isSameAs("Context test string")
