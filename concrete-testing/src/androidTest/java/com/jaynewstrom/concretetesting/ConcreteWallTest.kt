2f3374c23d96d1b2f598a123a0c6a8ed2cd26923
Jay Newstrom <jaynewstrom@gmail.com>
2020-06-09 10:07:05 -0500

    Fix deprecations. (#61)

--- concrete-testing/src/androidTest/java/com/jaynewstrom/concretetesting/ConcreteWallTest.kt
+++ concrete-testing/src/androidTest/java/com/jaynewstrom/concretetesting/ConcreteWallTest.kt
@@ -1,8 +1,8 @@
 package com.jaynewstrom.concretetesting
 
 import android.view.LayoutInflater
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.jaynewstrom.concrete.Concrete
 import org.fest.assertions.api.Assertions.assertThat
 import org.junit.Test
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith
 class ConcreteWallTest {
     @Test fun ensureContextCreatedFromWallCanBeUsedToInject() {
         val wall = Concrete.pourFoundation(DaggerContextTestComponent.create())
-        val context = wall.createContext(InstrumentationRegistry.getTargetContext())
+        val context = wall.createContext(ApplicationProvider.getApplicationContext())
         val target = ContextTestTarget()
         Concrete.getComponent<ContextTestComponent>(context).inject(target)
         assertThat(target.injectedString).isSameAs("Context test string")
@@ -21,7 +21,7 @@ class ConcreteWallTest {
     @Test fun whenViewIsCreatedWithLayoutInflaterFromChildContextEnsureItCanBeInjectedWithContext() {
         val wall = Concrete.pourFoundation(DaggerContextTestComponent.create())
         val child = wall.stack(ContextChildTestBlock(wall.component))
-        val context = child.createContext(InstrumentationRegistry.getTargetContext())
+        val context = child.createContext(ApplicationProvider.getApplicationContext())
         val exampleView = LayoutInflater.from(context).inflate(R.layout.example, null) as ExampleView
         assertThat(exampleView.injectedChildString).isSameAs("I'm the child")
         assertThat(exampleView.injectedString).isSameAs("Context test string")
